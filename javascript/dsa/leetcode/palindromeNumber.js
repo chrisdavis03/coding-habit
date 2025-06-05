907bb9bfaf337ed673ff2361346d6785e786ced6
chrisdavis03 <chrisdavis03@gmail.com>
2025-06-05 07:42:41 -0400

    js palendrome int

--- javascript/dsa/leetcode/palindromeNumber.js
+++ javascript/dsa/leetcode/palindromeNumber.js
@@ -5,12 +5,36 @@ Given an integer, return true if x is a palendrome and false otherwise.
 
 x could be between -2.147B and 2.147B
 
+
+# Thoughts
+# I feel like there is a pure path way to do this. but we could also convert to an array, reverse and traverse indexes. 
 */
 
-const isPalendrome = () => {
+const isPalendrome = (x) => {
+  let f = String(x);
+  f = f.split("").map(Number);
+  let b = f.toReversed();
 
-}
+  //   process.stdout.write(f + "\n" + b + "\n");
 
-const example1 = 121
-const example2 = -121
-const example3 = 10
+  let p = new Boolean();
+  for (i = 0; i < f.length / 2; i++) {
+    if (f[i] === b[i]) {
+      p = true;
+    } else {
+      p = false;
+      break;
+    }
+  }
+
+  return p;
+};
+
+const example1 = 121;
+const example2 = -121;
+const example3 = 10;
+
+process.stdout.write(isPalendrome(example1) + "\n");
+process.stdout.write(isPalendrome(example2) + "\n");
+process.stdout.write(isPalendrome(example3) + "\n");
+process.stdout.write(isPalendrome(123456654321) + "\n");
